f63f6a299536202b8c8be2aeef06cf8c48faba32
Pau Sanso <pausgil@gmail.com>
2021-10-25 19:59:40 +0200

    Lo dejo casi terminado

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -22,14 +22,14 @@ namespace Ahorcado
 
     public partial class MainWindow : Window
     {
-        //private readonly string[] palabras = { "Pausito", "Josekar", "Deluxeluisiete", "Pipo" };
-        private readonly string[] palabras = { "pipo", "pipo", "pipo", "pipo" };
+        private readonly string[] palabras = { "Pausito", "Josekar", "Deluxeluisiete", "Pipo", "Interfaces", "Electrodomestico", "StackPanel", "Hipopotamo" };
         private string palabraElegida;
+        int fallos, aciertos;
         public MainWindow()
         {
             InitializeComponent();
 
-            char[] letras = "abcdefghijklmnñopqrstuvwxyz".ToCharArray();
+            char[] letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".ToCharArray();
             int numLetra = 0;
 
             for (int fila = 0; fila < 3; fila++)
@@ -41,7 +41,7 @@ namespace Ahorcado
                     Button boton = new Button();
                     Grid.SetRow(boton, fila);
                     Grid.SetColumn(boton, columna);
-                    //boton.Click += LetraButton_Click;
+                    boton.Click += LetraButton_Click;
                     boton.Style = (Style)this.Resources["LetrasButtonStyle"];
                     boton.Content = letras[numLetra];
                     boton.Tag = letras[numLetra];
@@ -51,60 +51,59 @@ namespace Ahorcado
                 }
             }
 
-            palabraElegida = ObtenerPalabraAleatoria();
-            for (int i = 0; i < palabraElegida.Length; i++)
-            {
-                TextBlock tb = new TextBlock();
-                tb.Text = palabraElegida[i].ToString();
-                tb.Visibility = Visibility.Hidden;
-
-                Viewbox vb = new Viewbox();
-                vb.Child = tb;
-
-                Border b = new Border();
-                b.BorderThickness = new Thickness(0, 0, 0, 3);
-                b.BorderBrush = Brushes.Black;
-                b.Margin = new Thickness(10);
-                b.Width = 30;
-                b.Child = vb;
-
-                huecoStackpanel.Children.Add(b);
-
-            }
-
+            IniciarJuego();
+            
         }
         public string ObtenerPalabraAleatoria()
         {
             Random palabraAleatoria = new Random();
             return palabras[palabraAleatoria.Next(0, 3)];
         }
-        private void ComprobarLetra(char letra)
+        private void ComprobarLetra(string letra)
         {
-            IList huecoStackpanellist = huecoStackpanel.Children;
-
+            bool existe = false;
             for (int i = 0; i < huecoStackpanel.Children.Count; i++)
             {
-                Viewbox viewbox = (Viewbox)huecoStackpanellist[i];
-                TextBlock textblock = viewbox.Child as TextBlock;
-                if (textblock.Text.Equals(letra))
+                Border b = (Border)huecoStackpanel.Children[i];
+                Viewbox vb = b.Child as Viewbox;
+                TextBlock tb = vb.Child as TextBlock;
+                if (tb.Text.Equals(letra.ToLower()) || tb.Text.Equals(letra.ToUpper()))
                 {
-                    textblock.Visibility = Visibility.Visible;
+                    tb.Visibility = Visibility.Visible;
+                    existe = true;
+                    aciertos++;
+                    if (aciertos == huecoStackpanel.Children.Count) MessageBox.Show("Has Acertado!!", "Resultado");
                 }
-
-
             }
+            if (!existe)
+            {
+                fallos++;
+                cambiarImagen(fallos);
+                if (fallos == 10)
+                {
+                    AcabarJuego();
+                    MessageBox.Show("Has Fallado demasiadas veces, intentalo de nuevo", "RESULTADO");
+                }
+            }  
+        }
+
+        private void cambiarImagen(int fallos)
+        {
+            ahorcadoImagen.Source = new BitmapImage(new Uri(@".\assets\" + fallos + ".jpg", UriKind.Relative));
         }
 
         private void LetraButton_Click(object sender, RoutedEventArgs e)
         {
             Button letraButton = (Button)sender;
             char letra = (char)letraButton.Content;
-            ComprobarLetra(letra);
             letraButton.IsEnabled = false;
-        }
+            ComprobarLetra(letra.ToString());
 
-        private void NuevaPartidaButton_Click(object sender, RoutedEventArgs e)
+        }
+        private void IniciarJuego()
         {
+            fallos = 0;
+            aciertos = 0;
             palabraElegida = ObtenerPalabraAleatoria();
             for (int i = 0; i < palabraElegida.Length; i++)
             {
@@ -125,11 +124,42 @@ namespace Ahorcado
                 huecoStackpanel.Children.Add(b);
 
             }
+        }
+        private void NuevaPartidaButton_Click(object sender, RoutedEventArgs e)
+        {
+            huecoStackpanel.Children.Clear();
+            ahorcadoImagen.Source = new BitmapImage(new Uri(@".\assets\0.jpg", UriKind.Relative));
+            IniciarJuego();
             for (int i = 0; i < letrasGrid.Children.Count; i++)
             {
                 Button boton = (Button)letrasGrid.Children[i];
                 boton.IsEnabled = true;
             }
+            MessageBox.Show("Has iniciado una nueva partida", "RESULTADO");
+        }
+        private void AcabarJuego()
+        {
+            for (int i = 0; i < letrasGrid.Children.Count; i++)
+            {
+                Button boton = (Button)letrasGrid.Children[i];
+                boton.IsEnabled = false;
+                ahorcadoImagen.Source = new BitmapImage(new Uri(@".\assets\10.jpg", UriKind.Relative));
+            }
+
+            for (int i = 0; i < huecoStackpanel.Children.Count; i++)
+            {
+                Border b = (Border)huecoStackpanel.Children[i];
+                Viewbox vb = b.Child as Viewbox;
+                TextBlock tb = vb.Child as TextBlock;
+
+                tb.Visibility = Visibility.Visible;
+
+            }
+        }
+        private void RendirseButton_Click(object sender, RoutedEventArgs e)
+        {
+            AcabarJuego();
+            MessageBox.Show("Te has rendido", "RESULTADO");
         }
     }
 }
